63188de7b9feff729b1c0adcdc981022fc76a786
TaneriX07 <tanerivince@gmail.com>
2022-02-21 11:37:39 +0800

    Finish the logic

--- dna.py
+++ dna.py
@@ -1,19 +1,45 @@
 import csv
 import sys
 
-
 def main():
 
-    # TODO: Check for command-line usage
+    # Check for command-line usage
+    if len(sys.argv) < 3:
+        print("Usage: python dna.py *.csv *.txt")
+        sys.exit(1)
 
-    # TODO: Read database file into a variable
-    
-    # TODO: Read DNA sequence file into a variable
+    # Read database file into a variable
+    database = []
+    with open(sys.argv[1], 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            database.append(row)
 
-    # TODO: Find longest match of each STR in DNA sequence
+    # Read DNA sequence file into a variable
+    with open(sys.argv[2], 'r') as file:
+        dna_sequence = file.read()
 
-    # TODO: Check database for matching profiles
+    # List of subsequences to check
+    subsequences = list(database[0].keys())[1:]
 
+    # Find longest match of each STR in DNA sequence
+    result = {}
+    for subsequence in subsequences:
+        result[subsequence] = longest_match(dna_sequence, subsequence)
+
+    # Check database for matching profiles
+    for person in database:
+        match = 0
+        for subsequence in subsequences:
+            if int(person[subsequence]) == result[subsequence]:
+                match += 1
+
+        # If all subsequences matched
+        if match == len(subsequences):
+            print(person["name"])
+            return
+
+    print("No match")
     return
 
 
@@ -43,11 +69,11 @@ def longest_match(sequence, subsequence):
             # If there is a match in the substring
             if sequence[start:end] == subsequence:
                 count += 1
-            
+
             # If there is no match in the substring
             else:
                 break
-        
+
         # Update most consecutive matches found
         longest_run = max(longest_run, count)
 
@@ -56,3 +82,4 @@ def longest_match(sequence, subsequence):
 
 
 main()
+
